f1bec986ec286184f771f79ce5956d831876bcb0
PrTesla <t.totota@gmail.com>
2015-04-25 13:19:10 +0200

    PNJ, musique, UI et autres

--- Assets/Scripts/PNJ.cs
+++ Assets/Scripts/PNJ.cs
@@ -19,8 +19,12 @@ public class PNJ : MonoBehaviour {
     private GameObject leader;
     public float speed;
 
+    private Manager manager;
+    public bool evangelisable;
+
     void Awake()
     {
+        manager = GameObject.Find("_manager").GetComponent<Manager>();
         m_rigid = transform.GetComponent<Rigidbody2D>();
         leader = GameObject.Find("GroupeJoueur").GetComponent<CharacControl>().characters[0];
     }
@@ -32,29 +36,94 @@ public class PNJ : MonoBehaviour {
         dir.Normalize();
 
 
-        // Vers le leader
-        m_rigid.velocity = -dir * speed * Time.deltaTime;
-
-
-        //vers l'exterieur
-        //m_rigid.velocity = dir * speed * Time.deltaTime;
-
-
-
-
-
 
         if (PNJ_Status == PNJ_Type.Electro_FAN)
         {
-            
+            if (manager.activeRadio == 0) //electro
+            {
+                //aime
+                evangelisable = true;
+                transform.tag = "PNJ";
+                m_rigid.velocity = -dir * speed * Time.deltaTime;
+            }
+            else  if (manager.activeRadio == 1) //rock
+            {
+                //aime pas
+                evangelisable = false;
+                transform.tag = "PNJ";
+                m_rigid.velocity = dir * speed * Time.deltaTime;
+            }
+            else if (manager.activeRadio == 2) //HipHop
+            {
+                evangelisable = false;
+                transform.tag = "Background";
+                m_rigid.velocity = Vector2.zero;
+            }
+            else if (manager.activeRadio == 3) //NoRadio
+            {
+                evangelisable = false;
+                transform.tag = "Background";
+                m_rigid.velocity = Vector2.zero;
+            }
+                
         }
         else if (PNJ_Status == PNJ_Type.Rock_FAN)
         {
-            
+            if (manager.activeRadio == 0) //electro
+            {
+                evangelisable = false;
+                transform.tag = "Background";
+                m_rigid.velocity = Vector2.zero;
+            }
+            else if (manager.activeRadio == 1) //rock
+            {
+                //aime
+                evangelisable = true;
+                transform.tag = "PNJ";
+                m_rigid.velocity = -dir * speed * Time.deltaTime;
+            }
+            else if (manager.activeRadio == 2) //HipHop
+            {
+                evangelisable = false;
+                //aime pas
+                transform.tag = "PNJ";
+                m_rigid.velocity = dir * speed * Time.deltaTime;
+            }
+            else if (manager.activeRadio == 3) //NoRadio
+            {
+                evangelisable = false;
+                transform.tag = "Background";
+                m_rigid.velocity = Vector2.zero;
+            }
         }
         else if (PNJ_Status == PNJ_Type.HipHop_FAN)
         {
-            
+            evangelisable = false;
+            if (manager.activeRadio == 0) //electro
+            {
+                //aime pas
+                transform.tag = "PNJ";
+                m_rigid.velocity = dir * speed * Time.deltaTime;
+            }
+            else if (manager.activeRadio == 1) //rock
+            {
+                evangelisable = false;
+                transform.tag = "Background";
+                m_rigid.velocity = Vector2.zero;
+            }
+            else if (manager.activeRadio == 2) //HipHop
+            {
+                //aime
+                evangelisable = true;
+                transform.tag = "PNJ";
+                m_rigid.velocity = -dir * speed * Time.deltaTime;
+            }
+            else if (manager.activeRadio == 3) //NoRadio
+            {
+                evangelisable = false;
+                transform.tag = "Background";
+                m_rigid.velocity = Vector2.zero;
+            }
         }
     }
 }

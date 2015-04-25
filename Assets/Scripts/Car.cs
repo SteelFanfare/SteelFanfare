4ced4769fef2663793de19525a46f3a3756a2b41
Guillaume Olivetti <contact@devduweb.com>
2015-04-25 13:55:42 +0200

    car spawner

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public int lifetime = 2;
+	private float delay;
+
+	void Start() {
+		delay = lifetime;
+	}
+
+	void Update () {
+		if (delay > 0.0f) {
+			delay -= Time.deltaTime * 5;
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "PNJ") {
+			Debug.Log ("Squish");
+			Destroy(other);
+		}
+	}
+}
---
+++ Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour {
+
+	public float HautY = 2.50f;
+	public float BasY = -3.0f;
+	public int delay = 5;
+	public float carSpeed = 2;
+
+	public Rigidbody2D car;
+
+	private float recharge;
+
+	void Start() {
+		recharge = delay;
+	}
+	
+	void Update () {
+		if (recharge > 0.0f) {
+			recharge -= Time.deltaTime * 5;
+		} else {
+			SpawnCar();
+			recharge = delay;
+		}
+	}
+
+	void SpawnCar() {
+		bool isHaut = Random.value > 0.5f;
+
+		Vector3 position = new Vector3 (
+			isHaut ? 12.0f : -12.0f,
+			isHaut ? HautY : BasY
+		);
+		
+		Quaternion orientation = new Quaternion (
+			0,
+			0,
+			isHaut ? -180 : 0,
+			0
+		);
+
+		float speed = isHaut ? -carSpeed : carSpeed;
+
+		Rigidbody2D newCar = (Rigidbody2D)Instantiate (car, position, orientation);
+		newCar.velocity = new Vector2(speed, 0);
+	}
+}

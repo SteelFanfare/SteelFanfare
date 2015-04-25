3c11aa24c21b7cc7093070f9dfe1aa2ac77cdbb9
Guillaume Olivetti <contact@devduweb.com>
2015-04-25 11:23:30 +0200

    ajout collisions

--- Assets/Scripts/CaracManager.cs
+++ Assets/Scripts/CaracManager.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class CaracManager : MonoBehaviour {
 
+	void OnCollisionStay2D(Collision2D other)
+	{
+		Debug ("collision");
+		if (other.gameObject.tag == "Pick Up") {
+			other.gameObject.SendMessage ("OnPlayerHover");
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log ("collision!");
-		if (other.gameObject.tag == "Pick Up") {
-			other.gameObject.SendMessage("OnPlayerEnter");
-			other.gameObject.SetActive (false);
-		} else if (other.gameObject.tag == "Effector") {
+		if (other.gameObject.tag == "Effector") {
 			other.gameObject.SendMessage("OnPlayerEnter");
 		}
-	}	
+	}
+
+
 
 }
---
+++ Assets/Scripts/PickupManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupManager : MonoBehaviour {
+
+	public int hoverDelay = 2;
+	public int progressSpeed = 5;
+
+	private float progress = 0.0f;
+	private float recharge = 0.0f;
+	
+	// Update the progression when the player
+	void OnPlayerHover () 
+	{
+		if (recharge > 0.0f) {
+			return;
+		}
+
+		progress += progressSpeed * 100 * Time.deltaTime;
+
+		Debug.Log ("Pickup " + progress.ToString());
+		
+		recharge = hoverDelay;
+
+		if (progress >= 100.0f) {
+			Complete ();
+		}
+	}
+
+	void Complete()
+	{
+		Debug.Log ("Complete!");
+
+		this.gameObject.SetActive (false);
+	}
+
+	// Limit the calls of playerHover
+	void Update() {
+		if (recharge > 0.0f) {
+			//Debug.Log ("Recharge " + recharge.ToString());
+			recharge -= Time.deltaTime * 5;
+		}
+	}
+}

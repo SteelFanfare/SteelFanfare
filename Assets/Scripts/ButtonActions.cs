d50c88e7ea5439d67f4f9024bb0dce35a0e6607e
Guillaume Olivetti <contact@devduweb.com>
2015-04-25 22:55:35 +0200

    Scenes HUD

---
+++ Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonActions : MonoBehaviour {
+
+	public void goTutorial()
+	{
+		Application.LoadLevel ("tutorial");
+	}
+
+	public void goCredits()
+	{
+		Application.LoadLevel ("credits");
+	}
+
+	public void goTitle()
+	{
+		Application.LoadLevel ("titre");
+	}
+
+	public void startGame()
+	{
+		GameObject.Find ("ButtonStart").SetActive (false);
+
+		GameObject TextLoading = GameObject.Find ("LoadingText");
+		GameObject LoadingPosition = GameObject.Find ("LoadingPosition");
+
+		TextLoading.transform.position = LoadingPosition.transform.position;
+
+		Application.LoadLevel ("testScene");
+	}
+}

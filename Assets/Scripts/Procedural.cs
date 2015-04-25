71be477bb7b9a12b6b1fb10b46bb0aaa00c690f5
PrTesla <t.totota@gmail.com>
2015-04-25 18:39:09 +0200

    Ld et Procédural de base

---
+++ Assets/Scripts/Procedural.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Procedural : MonoBehaviour {
+
+    public GameObject[] blocs;
+    public GameObject spawner;
+
+    private int rand;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Background")
+        {
+            rand = Random.Range(0, blocs.Length);
+            Instantiate(blocs[rand], spawner.transform.position, Quaternion.identity);
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Background")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

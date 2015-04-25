597306d2d2a465629011f24e12816941a6268623
PrTesla <t.totota@gmail.com>
2015-04-25 18:58:17 +0200

    Procedural
    Avancement en Proceduram

--- Assets/Scripts/Procedural.cs
+++ Assets/Scripts/Procedural.cs
@@ -4,23 +4,32 @@ using System.Collections;
 public class Procedural : MonoBehaviour {
 
     public GameObject[] blocs;
-    public GameObject spawner;
+    public GameObject background;
+    public GameObject spawnerBG;
+    public GameObject spawnerSituations;
 
     private int rand;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Background")
+        if (other.tag == "Situation")
         {
             rand = Random.Range(0, blocs.Length);
-            Instantiate(blocs[rand], spawner.transform.position, Quaternion.identity);
-
+            Instantiate(blocs[rand], spawnerSituations.transform.position, Quaternion.identity);
+        }
+        if (other.tag == "Background")
+        {
+            Instantiate(background, spawnerBG.transform.position, Quaternion.identity);
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Background")
+        {
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "Situation")
         {
             Destroy(other.gameObject);
         }

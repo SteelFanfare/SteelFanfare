251e5a1eae9f5950477a82359a5e558345e56eec
Guillaume Olivetti <contact@devduweb.com>
2015-04-25 15:41:11 +0200

    perfs sur les pietons

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -21,8 +21,6 @@ public class Car : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "PNJ") {
-			Debug.Log ("Squish");
-
             other.gameObject.SendMessage("Squish");
 		}
 	}

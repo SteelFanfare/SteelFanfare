b8c9e872a82cbb79367804e83609006d351564a4
Guillaume Olivetti <contact@devduweb.com>
2015-04-26 09:54:31 +0200

    Limitation des mouvements à l'écran

--- Assets/Scripts/CharacControl.cs
+++ Assets/Scripts/CharacControl.cs
@@ -27,43 +27,50 @@ public class CharacControl : MonoBehaviour
         first = 0;
     }
 
+	int GetLeaderKey()
+	{
+		#region test Leader
+		if (characters[0].activeInHierarchy == true)
+		{
+			first = 0;
+		}
+		else if (characters[1].activeInHierarchy == true)
+		{
+			first = 1;
+		}
+		else if (characters[2].activeInHierarchy == true)
+		{
+			first = 2;
+		}
+		else
+		{
+			first = 3;
+		}
+		#endregion
+
+		return first;
+	}
+
 	void Update ()
     {
         #region valeurs a réassigner à chaque frame
         // car on l'utilise plusieur fois
         delta = Time.deltaTime;
 
-        if (invicibility > 0)
+        if (invicibility > 0) {
             invicibility -= delta;
+		}
 
         // axis de manettes entre -1 et 1
         m_velocity.x = Input.GetAxis("Horizontal"); 
         m_velocity.y = Input.GetAxis("Vertical");
         #endregion
 
-        #region test Leader
-        if (characters[0].activeInHierarchy == true)
-        {
-            first = 0;
-        }
-        else if (characters[1].activeInHierarchy == true)
-        {
-            first = 1;
-        }
-        else if (characters[2].activeInHierarchy == true)
-        {
-            first = 2;
-        }
-        else
-        {
-            first = 3;
-        }
-        #endregion
-
+		first = GetLeaderKey ();
 
         #region mouvement
         //on bouge le premier directement
-        characters[first].transform.GetComponent<Rigidbody2D>().velocity = m_velocity * speed * delta; 
+		characters[first].transform.GetComponent<Rigidbody2D>().velocity = m_velocity * speed * delta; 
 
         //les autres ont un retard
         for (int i = first+1; i < characters.Length; i++)
@@ -80,4 +87,19 @@ public class CharacControl : MonoBehaviour
         }
         #endregion
     }
+
+	void LateUpdate()
+	{
+		first = GetLeaderKey ();
+
+		float dist = (transform.position - Camera.main.transform.position).z;
+		Vector3 bottomLeft = Camera.main.ViewportToWorldPoint (new Vector3(0.0f, 0.0f, dist));
+		Vector3 topRight = Camera.main.ViewportToWorldPoint (new Vector3(1.0f, 1.0f, dist));
+		
+		characters [first].transform.position = new Vector3 (
+			Mathf.Clamp(characters [first].transform.position.x, bottomLeft.x, topRight.x),
+			Mathf.Clamp(characters [first].transform.position.y, bottomLeft.y, topRight.y),
+			characters [first].transform.position.z
+		);
+	}
 }
